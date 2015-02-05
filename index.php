6b9a1b2c2abe4a0815969ccc692e652938dd4666
Ruben Verweij <development@rubenverweij.nl>
2015-02-05 13:00:06 +0100

    Edited theme

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ define('ROOT_PATH', __DIR__);
 
 require_once(ROOT_PATH.'/vendor/autoload.php');
 
+ob_start("ob_gzhandler");
+
 $oCrispus = new Crispus\Crispus(ROOT_PATH.'/config/config.json');
 
 echo $oCrispus->render();
